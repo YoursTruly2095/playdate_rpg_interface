e09b1045bd8f620a7bdcb56eabf90bb6dc9f3497
Tony Park <tony@park-hagiwara.net>
2022-09-17 17:07:45 +0100

    Tidy up and add comments

--- RightCrankMenu.lua
+++ RightCrankMenu.lua
@@ -27,40 +27,36 @@ which can be found at https://github.com/ConfidentFloor6601/playdate_rpg_interfa
  * -----------------------------------------------------------------------------
  * (This is the zlib License)
 
+Please credit u/ConfidentFloor6601 and YoursTruly2095 for this work.
+
 --]]
 
 --[[
 
     Flexible RightCrankMenu
 
-    To use, just 'require' this file in your game code, and call
-    RightCrankMenu.load(), RightCrankMenu.draw(), and RightCrankMenu.update()
-    from their respective playdate / playdate functions.
+    To use, just 'import' this file in your game code, and call
+    RightCrankMenu.update() from playdate.update().
     
     To change the setup of the menu, change things in the 'Configuration' section below
     
     You can get the currently selected icon by calling 'get_current_icon()'. 
     
-    This code also requires a tiny 'globals' module with provides the screen 
-    width and height, and 'Crank' module which contains a virtual representation
-    of the playdate crank (to be replaced by the real thing quite soon I hope!)
-    
-    A useful future extension might be to include a function to call when an
-    icon is selected in the icon table, and to consume keypresses for selections.
-    
 --]]
 
-local RightCrankMenu = {}
+local RightCrankMenu = 
+{ 
+    menu_titles = {}, 
+    current_icon = nil, 
+    full_rotation_angle = 0,
+    active = true,
+    crank_angle = 0,
+    hidden = false,
+}
 
 -------------------------------------------------------------------------------------
 ------- CONFIGURATION ---------------------------------------------------------------
 -------------------------------------------------------------------------------------
-
--- Titles for the icons selected with the Right Crank Menu (RCM).
--- You can add or take away from here, although your menu will start to 
--- misbehave if you have too few to fill the screen. But in that case, you 
--- can make them bigger below.
-RightCrankMenu.menu_titles = {}
 
 -- Crank icon dimensions (including borders)
 -- You can set these freely (within reason).
@@ -79,15 +75,18 @@ RightCrankMenu.offset = 100
 -- Adjust in real-time if your player character quaffs too many potions of beer
 RightCrankMenu.icon_shift_angle = 60  
 
--- the click wheel menu is active by default
-RightCrankMenu.active = true
 
 -- if you change the start icon, you need to set the initial RightCrankMenu.angle to match
 RightCrankMenu.angle = 0
 
-RightCrankMenu.current_icon = nil 
-RightCrankMenu.full_rotation_angle = 0
+-------------------------------------------------------------------------------------
+------- END CONFIGURATION -----------------------------------------------------------
+-------------------------------------------------------------------------------------
 
+
+-------------------------------------------------------------------------------------
+------- API FUNCTIONS ---------------------------------------------------------------
+-------------------------------------------------------------------------------------
 function RightCrankMenu.register_icon(icon)
     
     -- set up defaults for options
@@ -174,118 +173,6 @@ function RightCrankMenu.register_selector(selector)
     RightCrankMenu.selector = selector
 end
 
--------------------------------------------------------------------------------------
-------- END CONFIGURATION -----------------------------------------------------------
--------------------------------------------------------------------------------------
-
-
-
--- this is internal use
-RightCrankMenu.crank_angle = 0
-
-RightCrankMenu.hidden = false
-
---[[
-function RightCrankMenu.load()
-    for index,value in ipairs(RightCrankMenu.menu_titles) do
-        RightCrankMenu.menu_titles[index]["icon"] = playdate.graphics.image.new(RightCrankMenu.menu_titles[index][2])
-        if RightCrankMenu.menu_titles[index][3] ~= "" then
-            RightCrankMenu.menu_titles[index]["icon_d"] = playdate.graphics.image.new(RightCrankMenu.menu_titles[index][3])
-        end
-        RightCrankMenu.menu_titles[index]["enabled"] = true 
-    end
-end
---]]
-
-local function calculate_animation_x(x)
-    local offscreen_x = playdate.display.getWidth()
-    local ratio = RightCrankMenu.get_animation_ratio()
-    if RightCrankMenu.animation_type == 'hide' then
-        x = x + (offscreen_x - x) * (ratio)
-    elseif RightCrankMenu.animation_type == 'show' then
-        x = x + (offscreen_x - x) * (1-ratio)
-    end
-    return x
-end
-
-function RightCrankMenu.draw()
-
-    if RightCrankMenu.hidden and not RightCrankMenu.animating then return end
-
-    -- Right Crank Menu Box Outlines and Icons
-    for index,value in ipairs(RightCrankMenu.menu_titles) do
-        local icon = "icon"
-        if not RightCrankMenu.menu_titles[index]["enabled"] then icon = "disabled_icon" end
-        RightCrankMenu.draw_icon(RightCrankMenu.menu_titles[index][icon],index-1)
-    end
-
-    -- "active icon" selector box here
-    if RightCrankMenu.active or RightCrankMenu.animating_selector then
-        
-        local w, h, x, y
-        local selector
-        local Src_W = playdate.display.getWidth()
-        
-        if RightCrankMenu.selector then
-            selector = RightCrankMenu.selector
-            w,h = selector:getSize()
-            x = Src_W-RightCrankMenu.Ico_W - (w-RightCrankMenu.Ico_W)/2
-            y = RightCrankMenu.offset - (h-RightCrankMenu.Ico_H)/2
-        else
-            w = RightCrankMenu.Ico_W
-            h = RightCrankMenu.Ico_H
-            x = Scr_W-RightCrankMenu.Ico_W
-            y = RightCrankMenu.offset
-        end
-        
-        if RightCrankMenu.animating_selector then
-            x = calculate_animation_x(x)
-        end
-        
-        if RightCrankMenu.selector then
-            playdate.graphics.image.draw(selector,x,y)
-        else
-            playdate.graphics.drawRect(x,y,w,h)
-            playdate.graphics.drawRect(x+1,y+1,w-2,h-2)
-        end
-    end
-end
-
-function RightCrankMenu.draw_icon(icon, order)
-  
-  local angle = RightCrankMenu.angle
-  local Scr_W, Scr_H = playdate.display.getSize()
-
-  angle = -angle -- invert controls
-
-  local function get_y_from_angle(angle, order)
-    local y = (order + (angle/RightCrankMenu.icon_shift_angle)) * RightCrankMenu.Ico_H
-    y = y + RightCrankMenu.offset
-    return y
-  end
-  
-  local y = get_y_from_angle(angle, order)
-  
-  if y > Scr_H then
-    -- if y is right off the bottom of the screen, we should recalculate it in case it is at the top
-    angle = angle - RightCrankMenu.full_rotation_angle
-    y = get_y_from_angle(angle, order)
-  elseif y+RightCrankMenu.Ico_H < 0 then
-    -- if y is right off the top of the screen, we should recalculate it in case it is at the bottom
-    angle = angle + RightCrankMenu.full_rotation_angle
-    y = get_y_from_angle(angle, order)
-  end
-  
-  local x = Scr_W - RightCrankMenu.Ico_W + ((RightCrankMenu.offset-y)^2)/1000
-  
-    if RightCrankMenu.animating then
-        x = calculate_animation_x(x)
-    end
-  
-  playdate.graphics.image.draw(icon,x,y)
-  
-end
-    
 
 function RightCrankMenu.set_active(active)
     RightCrankMenu.crank_angle = playdate.getCrankPosition()
@@ -294,66 +181,6 @@ end
 
 function RightCrankMenu.is_active(active)
     return RightCrankMenu.active
-end
-
-local debug_delta = 0
-
-function RightCrankMenu.update(dt)
-    
-    -- handle the crank input
-    if RightCrankMenu.is_active() then
-        local last_crank_angle = RightCrankMenu.crank_angle
-        RightCrankMenu.crank_angle = playdate.getCrankPosition()
-        local angle_delta = (RightCrankMenu.crank_angle - last_crank_angle)
-        
-        if math.abs(angle_delta) > 180 then
-            -- assume overflow and the crank went the short way to this new angle
-            if angle_delta > 0 then angle_delta = angle_delta - 360 else angle_delta = angle_delta + 360 end
-        end
-        
-        -- adjust delta based on the angle ratio for the current icon
-        local current = RightCrankMenu.get_active_icon()
-        if current then
-            angle_delta = angle_delta / current['shift_ratio']
-        end
-        
-        RightCrankMenu.angle = (RightCrankMenu.angle + angle_delta) % RightCrankMenu.full_rotation_angle
-        if angle_delta ~= 0 then
-            debug_delta = angle_delta
-        end
-    end        
-    
-    -- handle the show / hide animation
-    if RightCrankMenu.start_animation then
-        RightCrankMenu.animation_elapsed = 0
-        RightCrankMenu.start_animation = false
-        RightCrankMenu.animating = true
-    elseif RightCrankMenu.animating then
-        RightCrankMenu.animation_elapsed = RightCrankMenu.animation_elapsed + dt
-        if RightCrankMenu.animation_elapsed > RightCrankMenu.animation_time then
-            RightCrankMenu.animating = false
-            RightCrankMenu.animating_selector = false
-        end
-    end
-    
-    RightCrankMenu.draw()
-    
-end
-
-function RightCrankMenu.get_animation_ratio()
-    if not RightCrankMenu.animating then
-        return 1
-    else
-        return RightCrankMenu.animation_elapsed / RightCrankMenu.animation_time
-    end
-end
-
-    
-function RightCrankMenu.debug_print()
-    playdate.graphics.drawText("RCM angle: "..RightCrankMenu.angle,125,25)
-    playdate.graphics.drawText("FRA: "..RightCrankMenu.full_rotation_angle,125,50)
-    playdate.graphics.drawText("delta: "..debug_delta,125,75)
-    playdate.graphics.drawText("icon: "..RightCrankMenu.current_icon,125,100)
 end
 
 function RightCrankMenu.get_active_icon()
@@ -407,6 +234,204 @@ function RightCrankMenu.show(mode)
     RightCrankMenu.animation_time = 0.33       -- seconds
     RightCrankMenu.start_animation = true
     RightCrankMenu.animation_type = 'show'
+end
+
+-------------------------------------------------------------------------------------
+------- END API FUNCTIONS -----------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------------
+------- UPDATE ----------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+--[[
+
+Call this from playdate.update().
+
+'dt' is 'delta time'. This is really a hang-over from Love2d, which passes you the 
+amount of time since the last update in the call to love.update().
+
+Playdate handles things slightly differently, by attempting to call update absolutley 
+regularly, at the specified frame rate. This defaults to 30fps, so for a game that
+isn't stretching the hardware, you can just pass 1/30 for the 'dt' argument and all
+should be well. If you decide to alter the playedate frame rate, adjust accordingly.
+If you find yourself with a variable framerate, you can apparently calculate the real 
+delta time using playdate.getCurrentTimeMilliseconds(), and RightCrankMenu should
+work happily.
+
+--]]
+
+function RightCrankMenu.update(dt)
+    
+    -- handle the crank input
+    if RightCrankMenu.is_active() then
+        local last_crank_angle = RightCrankMenu.crank_angle
+        RightCrankMenu.crank_angle = playdate.getCrankPosition()
+        local angle_delta = (RightCrankMenu.crank_angle - last_crank_angle)
+        
+        if math.abs(angle_delta) > 180 then
+            -- assume overflow and the crank went the short way to this new angle
+            if angle_delta > 0 then angle_delta = angle_delta - 360 else angle_delta = angle_delta + 360 end
+        end
+        
+        -- adjust delta based on the angle ratio for the current icon
+        local current = RightCrankMenu.get_active_icon()
+        if current then
+            angle_delta = angle_delta / current['shift_ratio']
+        end
+        
+        RightCrankMenu.angle = (RightCrankMenu.angle + angle_delta) % RightCrankMenu.full_rotation_angle
+        if angle_delta ~= 0 then
+            debug_delta = angle_delta
+        end
+    end        
+    
+    -- handle the show / hide animation
+    if RightCrankMenu.start_animation then
+        RightCrankMenu.animation_elapsed = 0
+        RightCrankMenu.start_animation = false
+        RightCrankMenu.animating = true
+    elseif RightCrankMenu.animating then
+        RightCrankMenu.animation_elapsed = RightCrankMenu.animation_elapsed + dt
+        if RightCrankMenu.animation_elapsed > RightCrankMenu.animation_time then
+            RightCrankMenu.animating = false
+            RightCrankMenu.animating_selector = false
+        end
+    end
+    
+    RightCrankMenu.draw()
+    
+end
+
+
+-------------------------------------------------------------------------------------
+------- DRAW ------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+--[[ 
+
+As far as playdate is concerned, draw() is an internal function, and to fit with the
+playdate design this function is called from RightCrankMenu.update() (above). If you're 
+happy with this or don't know what I'm on about, stop reading, it's fine :-)
+
+However, if you have code that is structured as per Love2d and you were suprised to find
+that there was no playdate.draw() callback, you might benefit from being able to do the
+update and the draw of the menu separately. In that case, just comment out the call to 
+RightCrankMenu.draw() at the bottom of RightCrankMenu.update(), and call 
+RightCrankMenu.draw() directly from your code as appropriate.
+
+--]]
+
+function RightCrankMenu.draw()
+
+    if RightCrankMenu.hidden and not RightCrankMenu.animating then return end
+
+    -- Right Crank Menu Box Outlines and Icons
+    for index,value in ipairs(RightCrankMenu.menu_titles) do
+        local icon = "icon"
+        if not RightCrankMenu.menu_titles[index]["enabled"] then icon = "disabled_icon" end
+        RightCrankMenu.draw_icon(RightCrankMenu.menu_titles[index][icon],index-1)
+    end
+
+    -- "active icon" selector box here
+    if RightCrankMenu.active or RightCrankMenu.animating_selector then
+        
+        local w, h, x, y
+        local selector
+        local Src_W = playdate.display.getWidth()
+        
+        if RightCrankMenu.selector then
+            selector = RightCrankMenu.selector
+            w,h = selector:getSize()
+            x = Src_W-RightCrankMenu.Ico_W - (w-RightCrankMenu.Ico_W)/2
+            y = RightCrankMenu.offset - (h-RightCrankMenu.Ico_H)/2
+        else
+            w = RightCrankMenu.Ico_W
+            h = RightCrankMenu.Ico_H
+            x = Scr_W-RightCrankMenu.Ico_W
+            y = RightCrankMenu.offset
+        end
+        
+        if RightCrankMenu.animating_selector then
+            x = calculate_animation_x(x)
+        end
+        
+        if RightCrankMenu.selector then
+            playdate.graphics.image.draw(selector,x,y)
+        else
+            playdate.graphics.drawRect(x,y,w,h)
+            playdate.graphics.drawRect(x+1,y+1,w-2,h-2)
+        end
+    end
+end
+
+-------------------------------------------------------------------------------------
+------- INTERNALS -------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+local function calculate_animation_x(x)
+    local offscreen_x = playdate.display.getWidth()
+    local ratio = RightCrankMenu.get_animation_ratio()
+    if RightCrankMenu.animation_type == 'hide' then
+        x = x + (offscreen_x - x) * (ratio)
+    elseif RightCrankMenu.animation_type == 'show' then
+        x = x + (offscreen_x - x) * (1-ratio)
+    end
+    return x
+end
+
+
+function RightCrankMenu.draw_icon(icon, order)
+  
+  local angle = RightCrankMenu.angle
+  local Scr_W, Scr_H = playdate.display.getSize()
+
+  angle = -angle -- invert controls
+
+  local function get_y_from_angle(angle, order)
+    local y = (order + (angle/RightCrankMenu.icon_shift_angle)) * RightCrankMenu.Ico_H
+    y = y + RightCrankMenu.offset
+    return y
+  end
+  
+  local y = get_y_from_angle(angle, order)
+  
+  if y > Scr_H then
+    -- if y is right off the bottom of the screen, we should recalculate it in case it is at the top
+    angle = angle - RightCrankMenu.full_rotation_angle
+    y = get_y_from_angle(angle, order)
+  elseif y+RightCrankMenu.Ico_H < 0 then
+    -- if y is right off the top of the screen, we should recalculate it in case it is at the bottom
+    angle = angle + RightCrankMenu.full_rotation_angle
+    y = get_y_from_angle(angle, order)
+  end
+  
+  local x = Scr_W - RightCrankMenu.Ico_W + ((RightCrankMenu.offset-y)^2)/1000
+  
+    if RightCrankMenu.animating then
+        x = calculate_animation_x(x)
+    end
+  
+  playdate.graphics.image.draw(icon,x,y)
+  
+end
+    
+local debug_delta = 0
+
+
+function RightCrankMenu.get_animation_ratio()
+    if not RightCrankMenu.animating then
+        return 1
+    else
+        return RightCrankMenu.animation_elapsed / RightCrankMenu.animation_time
+    end
+end
+
+    
+function RightCrankMenu.debug_print()
+    playdate.graphics.drawText("RCM angle: "..RightCrankMenu.angle,125,25)
+    playdate.graphics.drawText("FRA: "..RightCrankMenu.full_rotation_angle,125,50)
+    playdate.graphics.drawText("delta: "..debug_delta,125,75)
+    playdate.graphics.drawText("icon: "..RightCrankMenu.current_icon,125,100)
 end
 
 return RightCrankMenu
